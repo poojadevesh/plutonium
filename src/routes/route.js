d4a3d18c782b0871a7b49320594069a7ad282122
poojadevesh <poojayadav.devesh@gmail.com>
2022-08-08 16:19:05 +0530

    add new code

--- src/routes/route.js
+++ src/routes/route.js
@@ -13,8 +13,18 @@ router.get('/test-you', function(req, res){
     res.send('This is the second routes implementation')
 })
 
-router.get('/give-me-students-data',function(req, res){
+router.get('/student-details/:name/:age',function(req, res){
+    // let student =["pooja","devesh","praveen"]
+// console.log("this is request"+req.path)
+console.log("this is request"+JSON.stringify(req.params))
+let reqParams= req.params
+let studentName =reqParams.name
+console.log(studentName)
 
+let studentDetails =studentName+" "+studentName
+
+
+    res.send("studentDetails")
 })
 module.exports = router;
 // adding this comment for no reason
