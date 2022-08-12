e0d07107584cdb33222a8b7ad386e39dbde0ff9f
poojadevesh <poojayadav.devesh@gmail.com>
2022-08-12 10:34:43 +0530

    add new code

--- src/routes/route.js
+++ src/routes/route.js
@@ -70,4 +70,121 @@ router.post("/test-post-4", function(req, res) {
     res.send(  { msg: arr , status: true }  )
 })
 
+router.get('/students/:name/:gender',function(req,res){
+    let studentName= req.params.name
+    console.log(studentName)
+    res.send(studentName)
+})
+
+// router.get('/get-query1',function(req,res){
+//     let marks =req.query.marks
+//     let result=marks>40?"pass":"fail"
+//     res.send({data:result,status:true})
+// })
+
+
+
+router.post('/post-query1',function(req,res){
+    let marks =req.query.marks
+    let result=marks>40?"pass":"fail"
+    res.send({data:result,status:true})
+})
+let arr =[22,3344,223,55,66,766,87,65,21,575665]
+router.post('/post-query2', function(req,res){
+    let input = req.query.input
+    let finalarr=arr.filter(ele=>ele>input)
+    res.send({data:finalarr,status:true})
+})
+
+let persons=[{
+    name:"pk",
+    age:10,
+    votingStatus:false
+},
+{
+    name:"sk",
+    age:20,
+    votingStatus:false
+},
+{
+    name:"AA",
+    age:70,
+    votingStatus:false
+},
+{
+    name:"SC",
+    age:5,
+    votingStatus:false
+},{
+    name:"HO",
+    age:40,
+    votingStatus:false
+}]
+
+router.post('/persons',function(req,res){
+
+  let votingAge = req.query.votingAge
+  let result =[]
+  for(let i=0;i<persons.length;i++){
+    let id =persons[i]
+    if(id.age>votingAge){
+        id.votingStatus=true
+        result.push(id)
+    }
+  }
+      return res.send({data:result,status:true})
+})
+
+
+    let players=  [
+    {
+        "name": "manish",
+        "dob": "1/1/1995",
+        "gender": "male",
+        "city": "jalandhar",
+        "sports": [
+            "swimming"
+        ]
+    },
+    {
+        "name": "gopal",
+        "dob": "1/09/1995",
+        "gender": "male",
+        "city": "delhi",
+        "sports": [
+            "soccer"
+        ]
+    },
+    {
+        "name": "lokesh",
+        "dob": "1/1/1990",
+        "gender": "male",
+        "city": "mumbai",
+        "sports": [
+            "soccer"
+        ]
+    },
+]
+
+
+router.post('/players', function (req, res) {
+    let newname=req.body.name
+    num =0
+    for(let i =0; i <players.length;i++){
+        let data=players[i]
+        if(data.name===newname){
+            let num=1
+            res.send('try another name')
+        }
+        if(num==0){
+            players.push(req.body)
+
+    }
+}
+    res.send(players)
+});
+
+ 
 module.exports = router;
+
+
