efb09015e6790753d492b87c24283a38476299de
ajijsk <ajijsk240@gmail.com>
2022-09-20 16:04:54 +0530

    update

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -6,8 +6,118 @@ import { } from '../util/validator.js'
 
 
 
+//<=============================================== book/put ==============================>
+
+
+const updateBook = async (req, res) => {
+    try {
+      let bookId = req.params.bookId;
+  
+      if (!ObjectId.isValid(bookId)) {
+        return res.status(400).send({ status: false, message: "Book Id is Invalid" });
+      }
+  
+      let bookDetailToUpdate = req.body;
+      if (Object.keys(bookDetailToUpdate).length === 0) {
+        return res.status(400).send({ status: false, message: 'Please Provide Details for Update' })
+      }
+  
+      let { title, excerpt, releasedAt, ISBN } = bookDetailToUpdate;
+  
+      let bookObject = {};
+      if (Object.keys(bookDetailToUpdate).indexOf('title') !== -1) {
+        if (title.trim().length === 0) {
+          return res.status(400).send({ status: false, message: 'Please Enter title' })
+        }
+       
+        let trimmedTitle = title
+          .trim()
+          .split(" ")
+          .filter((word) => word)
+          .join(" ")
+        const isTitleExist = await bookModel.findOne({ title: trimmedTitle });
+        if (isTitleExist) {
+          return res
+            .status(409)
+            .send({ status: false, message: "Title already exists" });
+        }
+        bookObject.title = trimmedTitle;
+      }
+    
+      if (Object.keys(bookDetailToUpdate).indexOf('excerpt') !== -1) {
+        if (excerpt.length == 0) {
+          return res.status(400).send({ status: false, message: 'Please Enter excerpt' })
+        }
+        bookObject.excerpt = excerpt
+          .trim()
+          .split(" ")
+          .filter((word) => word)
+          .join(" ");
+      }
+      if (Object.keys(bookDetailToUpdate).indexOf("ISBN") !== -1) {
+        if (ISBN.length == 0) {
+          return res.status(400).send({ status: false, message: 'Please Enter ISBN' })
+        }
+        let trimmedISBN = ISBN.trim()
+          .split("-")
+          .filter((word) => word)
+          .join("");
+  
+       
+  
+        const isIsbnExist = await bookModel.findOne({ ISBN: trimmedISBN });
+        if (isIsbnExist) {
+          return res.status(409).send({ status: false, message: "ISBN already exists" });
+        }
+        bookObject.ISBN = trimmedISBN;
+  
+      }
+  
+      if (Object.keys(bookDetailToUpdate).indexOf("releasedAt") !== -1) {
+        
+        if (!(releasedAt)) {
+       
+          return res
+            .status(400)
+            .send({ status: false, message: "Enter Date in YYYY-MM-DD format!!!" });
+        }
+        
+      if (!(/^((\d{4}[\/-])(\d{2}[\/-])(\d{2}))$/.test(releasedAt))) {
+       
+        return res
+          .status(400)
+          .send({ status: false, message: "Enter Date in YYYY-MM-DD format!!!" });
+      }
+    
+      bookObject.releasedAt = releasedAt
+    }
+  
+  
+  
+      let book = await bookModel.findOne({ _id: bookId, isDeleted: false });
+      if (!book) {
+        return res
+          .status(404)
+          .send({
+            status: false,
+            message: 'Book not found',
+          });
+      }
+      
+      let updatedbookDetail = await bookModel.findOneAndUpdate(
+        { _id: book._id },
+        bookObject,
+        { returnDocument: "after" }
+      );
+  
+      return res
+        .status(200)
+        .send({ status: true, message: "Success", data: updatedbookDetail });
+    } catch (err) {
+      return res.status(500).send({ status: false, message: err.message });
+    }
+  };
 
 
 
-
-export {  }
+export {updateBook,  }
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
 import user from  '../models/userModel.js'
 
-import { } from '../util/validator.js'
+import {isValid } from '../util/validator.js'
 
 const createUser = async (req,res)=>{}
 
@@ -28,12 +28,74 @@ const createUser = async (req,res)=>{}
 
 
 //========================POST /login===============================
+const userLogin=async (req,res)=>{
+const { email, password } = req.body;
+
+if (Object.keys(req.body).length == 0) {
+    return res
+        .status(400)
+        .send({ status: false, message: "Enter Login Credentials." })
+}
+
+if (!isValid(email)) {
+    return res.status(400).send({ status: false, msg: "Email Required." })
+}
+if (!isValid(password)) {
+    return res.status(400).send({ status: false, msg: "Password Required." })
+}
+
+const validateEmail = function (mail) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+        return true;
+    }
+}
+
+if (!validateEmail(email)) {
+    return res
+        .status(400)
+        .send({ status: false, message: "Incorrect Email !!!" })
+}
+
+const validatePassword = function (password) {
+    if (/^[A-Za-z\W0-9]{8,15}$/.test(password)) {
+        return true;
+    }
+}
+
+if (!validatePassword(password)) {
+    return res
+        .status(400)
+        .send({ status: false, message: "Incorrect Password !!!" })
+}
+
+let user = await userModel
+    .findOne({ email: email, password: password })
+    .select({ _id: 1 })
+
+if (!user) {
+    return res.status(401).send({
+        status: false,
+        message: "Authentication failed!!!, Incorrect Email or Password !!!",
+    })
+}
+
+let token = jwt.sign(
+    {
+        userId: user._id,
+        iat: Math.floor(Date.now() / 1000),
+        exp: Math.floor((Date.now() / 1000) + 180 * 60),
+    },
+    "Room 36"
+)
+return res
+    .status(200)
+    .send({ status: true, message: "Login Successfully", token: token });
+};
 
 
 
 
 
 
-
-export {  }
+export { userLogin, }
 
