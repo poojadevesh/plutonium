2134df0ade578e3faccc13834c1e54e78ebe01c4
Richard-vinu <richardnova1999@gmail.com>
2022-09-20 12:52:08 +0530

    ES^

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,39 @@
-import bookModel from  '../models/bookModel.js'
+import user from  '../models/userModel.js'
 
 import { } from '../util/validator.js'
 
 const createUser = async (req,res)=>{}
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//========================POST /login===============================
+
+
+
+
+
+
+
 export {  }
+
