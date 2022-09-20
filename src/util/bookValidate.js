693b1ccc8dd7003b79a5bb94ad63413a775afb1b
Richard-vinu <richardnova1999@gmail.com>
2022-09-20 17:30:31 +0530

    bookvlidate

---
+++ src/util/bookValidate.js
@@ -0,0 +1,74 @@
+import mongoose from 'mongoose'
+
+// ------------dataValidation-----------
+const dataValidation = (data) => {
+    if (Object.keys(data).length != 0 )
+        return true
+    return false
+}
+
+//--------------------mongoDbId------------------------
+const isValidObjectId = (id) => {
+    return mongoose.Types.ObjectId.isValid(id)
+};
+
+//-----------------------phone-------------------------
+const isValidPhone = (mobile) => {
+    const ph = mobile.trim()
+    if (typeof ph == "string" && ph.match(/^[ 0-9 ]{10,10}$/))
+        return true
+    return false
+}
+// -----------------------------------email-------------------------------------
+const isValidEmail = (email) => {
+    if (typeof email == "string" && email.match(/^([a-z0-9_.]+@[a-z]+\.[a-z]{2,3})?$/))
+        return true
+    return false
+}
+
+//--------------------------------------password----------------------------------------
+const isValidPass = (password) => {
+    const regx = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,99}$/.test(password)
+    return regx
+};
+
+//-----------------isValidTitleEnum--------------------
+const isValidTitleEnum = (title) => {
+    return ["Mr", "Mrs", "Miss"].indexOf(title) !== -1
+};
+
+//---------------------------------------validText-------------------------------------------
+const isValidText = (text) => {
+    if (typeof text == "string" && text.trim().length != 0 && text.match(/^[a-zA-Z.]{2,}$/i))
+        return true
+    return false
+}
+
+//----------------------------------------name--------------------------------------------
+const isValidName = (name) => {
+    if ((typeof name == "String" && name.trim().length != 0 || name.match(/^[A-Za-z]{2,}$/)))
+        return true
+    return false
+};
+//-------------------------------reviews----------------------------
+const isValidReviews = (review) => {
+    const rev = review.trim()
+    if (typeof rev == "Number" && rev.match(/^[ 0-9 ]{1,1}$/))
+        return true
+    return false
+}
+//-------------------------------ISBN----------------------------
+const isValidIsbn = (value) => {
+    const isbn = value.trim()
+    if (typeof isbn == "string" && isbn.match(/^[ 0-9_- ]{13,13}$/))
+        return true
+    return false
+}
+
+// const isValidIsbn = function (value) {
+//     if (!(/^(?:ISBN(?:-1[03])?:? )?(?=[-0-9 ]{17}$|[-0-9X ]{13}$|[0-9X]{10}$)(?:97[89][- ]?)?[0-9]{1,5}[- ]?(?:[0-9]+[- ]?){2}[0-9X]$/.test(value.trim()))) {
+//         return false
+//     }
+//     return true
+// };
+export { dataValidation, isValidObjectId, isValidPhone, isValidEmail, isValidPass, isValidTitleEnum, isValidText, isValidName, isValidReviews, isValidIsbn  }
