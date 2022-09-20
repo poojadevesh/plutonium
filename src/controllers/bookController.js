b9db55d13be3c1ee211c80fa11af2aed2c7a4839
Richard-vinu <richardnova1999@gmail.com>
2022-09-20 17:30:59 +0530

    bookvlidate

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,6 +1,6 @@
 import bookModel from '../models/bookModel.js'
 import userModel from '../models/userModel.js'
-import { dataValidation, isValidObjectId, isValidPhone, isValidEmail, isValidPwd, isValidTitleEnum, isValidText, isValidName, isValidReviews, isValidIsbn } from '../util/userValidate.js'
+import { dataValidation, isValidObjectId, isValidPhone, isValidEmail, isValidPwd, isValidTitleEnum, isValidText, isValidName, isValidReviews, isValidIsbn } from '../util/bookValidate.js'
 
 // -----------------data present or not or extra in the body-------------------
 const createBook = async (req, res) => {
