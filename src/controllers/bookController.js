34be391ba733f9236144b51c9f5fb6fbce898fc5
Shayan Biswas <106047915+shbis@users.noreply.github.com>
2022-09-20 15:09:10 +0530

    validator updated

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,12 +1,16 @@
 import bookModel from '../models/bookModel.js'
 import userModel from '../models/userModel.js'
-import { } from '../util/validator.js'
+import { dataValidation, isValidObjectId, isValidPhone, isValidEmail, isValidPass, isValidTitleEnum, isValidText, isValidName, isValidReviews, isValidIsbn } from '../util/validator.js'
 
-
+// -----------------data present or not or extra in the body-------------------
 const createBook = async (req, res) => {
     try {
         const reqBody = req.body
         const { title, excerpt, userId, ISBN, category, subcategory, reviews } = reqBody
+
+        if (!dataValidation(reqBody))
+            return res.status(400).send({ status: false, message: 'Please fill the data in the correct way' })
+        
         if (!title)
             return res.status(400).send({ status: false, message: 'title isn\'t present' })
         if (!excerpt)
@@ -36,6 +40,24 @@ const createBook = async (req, res) => {
 
 
 
+<<<<<<< Updated upstream
+=======
+title, excerpt,
+userId
+category, subcategory
+reviews, ISBN
+
+
+
+
+
+
+### DELETE / books /: bookId / review /: reviewId
+    - Check if the review exist with the reviewId.Check if the book exist with the bookId.Send an error response with appropirate status code like[this](#error - response - structure) if the book or book review does not exist
+        - Delete the related reivew.
+- Update the books document - decrease review count by one
+
+>>>>>>> Stashed changes
 
 
 
--- src/util/validator.js
+++ src/util/validator.js
@@ -1,34 +1,68 @@
 const mongoose = require('mongoose')
-// ----------------dataValidation----------------
+
+// ------------dataValidation-----------
 const dataValidation = (data) => {
-    if (Object.keys(data).length != 0)
+    if (Object.keys(data).length != 0 || Object.keys(data).length > 7)
         return true
     return false
 }
 
-//------------------------------mongoDbId-------------------------------
+//--------------------mongoDbId------------------------
 const isValidObjectId = (mongoDbObjId) => {
     return mongoose.Types.ObjectId.isValid(mongoDbObjId)
 };
 
-// ----------------------------------------email------------------------------------
-const email = (email) => {
+//-----------------------phone-------------------------
+const isValidPhone = (mobile) => {
+    const ph = mobile.trim()
+    if (typeof ph == "string" && ph.match(/^[ 0-9 ]{10,10}$/))
+        return true
+    return false
+}
+// -----------------------------------email-------------------------------------
+const isValidEmail = (email) => {
     if (typeof email == "string" && email.match(/^([a-z0-9_.]+@[a-z]+\.[a-z]{2,3})?$/))
         return true
     return false
 }
-//-------------------------------password---------------------------------
+
+//--------------------------------------password----------------------------------------
 const isValidPass = (password) => {
     const regx = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,99}$/.test(password)
     return regx
 };
-//-----------------------------isValidTitleEnum---------------------------
+
+//-----------------isValidTitleEnum--------------------
 const isValidTitleEnum = (title) => {
     return ["Mr", "Mrs", "Miss"].indexOf(title) !== -1
 };
 
+//---------------------------------------validText-------------------------------------------
+const isValidText = (text) => {
+    if (typeof text == "string" && text.trim().length != 0 && text.match(/^[a-zA-Z.]{2,}$/i))
+        return true
+    return false
+}
 
-export { dataValidation, isValidObjectId, email, isValidPass, isValidTitleEnum }
-
-
+//----------------------------------------name--------------------------------------------
+const isValidName = (name) => {
+    if ((typeof name == "String" && name.trim().length != 0 || name.match(/^[A-Za-z]{2,}$/)))
+        return true
+    return false
+};
+//-------------------------------reviews----------------------------
+const isValidReviews = (review) => {
+    const rev = review.trim()
+    if (typeof rev == "Number" && rev.match(/^[ 0-9 ]{1,1}$/))
+        return true
+    return false
+}
+//-------------------------------ISBN----------------------------
+const isValidIsbn = (value) => {
+    const isbn = value.trim()
+    if (typeof isbn == "string" && isbn.match(/^[ 0-9_- ]{13,13}$/))
+        return true
+    return false
+}
+export { dataValidation, isValidObjectId, isValidPhone, isValidEmail, isValidPass, isValidTitleEnum, isValidText, isValidName, isValidReviews, isValidIsbn  }
 
