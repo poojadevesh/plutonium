3b22045aca8a559fe71e9497f82db0f22071cd1e
Richard-vinu <richardnova1999@gmail.com>
2022-09-20 15:44:17 +0530

    user

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,12 @@
 import express from 'express'
 const router = express.Router()
 import { createUser } from '../controllers/userController.js'
-//import { createBook } from '../controllers/bookController.js'
+import { createBook } from '../controllers/bookController.js'
 
 
 
 
-//router.post('/books', createBook)
+router.post('/books', createBook)
 
 
 router.post('/createUser',createUser)
