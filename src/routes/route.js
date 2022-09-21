ff6ed81e2c090970adae769f5e31c8027f97a4e2
Richard-vinu <richardnova1999@gmail.com>
2022-09-21 10:55:18 +0530

    addreview

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,16 +1,10 @@
 import express from 'express'
 const router = express.Router()
-<<<<<<< HEAD
 import { createUser,userLogin } from '../controllers/userController.js'
-import { createBook, getBook } from '../controllers/bookController.js'
+import { createBook, getBook,deleteBook } from '../controllers/bookController.js'
 import {addReview} from '../controllers/reviewController.js'
-=======
-import { createUser } from '../controllers/userController.js'
-import { createBook, getBook, deleteBook } from '../controllers/bookController.js'
-// import { updateReview } from '../controllers/reviewController.js'
->>>>>>> c0761bf13e9dc8f4f9fd616348cea9cf78d94291
 
-
+// User API
 router.post('/createUser',createUser)
 router.post('/login',userLogin)
 
@@ -21,12 +15,8 @@ router.delete('/books/:bookId', deleteBook)
 // router.delete('/books/:bookId/review:reviewId', updateReview)
 
 
-<<<<<<< HEAD
 // Review API
 router.post('/books/:bookId/review',addReview)
 
 
 export default router
-=======
-export default router
->>>>>>> c0761bf13e9dc8f4f9fd616348cea9cf78d94291
