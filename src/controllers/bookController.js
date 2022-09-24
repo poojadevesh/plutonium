1a35e288203a0a6e57907f342dc0acbe70dc584f
ziyaurrahman01 <ziyaurrahmankhan01@gmail.com>
2022-09-24 23:06:27 +0530

    updateCode

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -86,24 +86,25 @@ const createBook = async (req, res) => {
 //=====================================================================================================
 //GET /books BY-QUERY
  
-const getBooksQuery = async (req, res) => {
+const getBooksQuery = async function (req, res) {
   try {
-    const reqBody = req.query;
-    const { userId, category, subcategory } = reqBody
-    
-    if ((Object.keys(reqBody).length === 0) || (userId || category || subcategory)) {
-      
-      const books = await bookModel.find({ $and: [{ isDeleted: false }, reqBody] }).select({ title: 1, excerpt: 1, userId: 1, category: 1, releasedAt: 1, reviews: 1 }).sort({ title: 1 });
+      let data = req.query
+      let filter = { isDeleted: false, ...data };
+      if (Object.keys(data).length == 0) return res.status(400).send({status:false, message: "please inter data in query"})
+      // if (req.query.hasOwnProperty('userId')) {
+      //     if (!validator.isValidObjectId(req.query.userId)) return res.status(400).send({ status: false, message: "please enter the valid UserId...!" })
+      // }
 
-      if(books.length === 0)
-        return res.status(404).send({ status: false, message: `Book is\'nt found.` });
+      let findBooks = await bookModel.find(filter).select({ title: 1, excerpt: 1, userId: 1, category: 1, releasedAt: 1, reviews: 1, _id: 1 }).sort({ title: 1 })
+      if (!findBooks) return res.status(404).send({ status: false, msg: "No Book found" })
+      if (findBooks.length == 0) return res.status(404).send({ status: false, msg: "please enter existing Book" })
 
-      return res.status(200).send({ status: true, message: 'Books list', data: books });
-    } else  return res.status(400).send({ status: false, message: 'Invalid query.' });
+      return res.status(200).send({ status: true, message: 'Books list', data: findBooks })
 
-  } catch (err) {
-    res.status(500).send({ status: false, error: err.message });
-  }};
+  }
+  catch (err) { return res.status(500).send({ status: false, message: err.message }); }
+};
+
 
 //============================================================================================================================================
 //GET /books/:bookId 
