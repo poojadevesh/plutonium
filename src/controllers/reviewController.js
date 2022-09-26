9019741eecf8e292c7de86379389d06363c2c93f
ziyaurrahman01 <ziyaurrahmankhan01@gmail.com>
2022-09-26 09:58:38 +0530

    update

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -49,7 +49,7 @@ const addReview = async (req, res) => {
         let finalOp = await reviewModel.findOne({ _id: result._id }).select({ isDeleted: 0, createdAt: 0, updatedAt: 0, __v: 0 })
         finalOp.reviewedAt = new Date()
 
-        res.status(201).send({ status: true, message: `New Review added Sucessfully to the Book📕 '${findBook.title}'`, data: finalOp })
+        res.status(201).send({ status: true, message: `New Review added Sucessfully to the Book '${findBook.title}'`, data: finalOp })
     } catch (err) {
         return res.status(500).send({ status: false, message: err.message })
     }}
