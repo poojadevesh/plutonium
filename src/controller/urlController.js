ba3e9f3693a50dca20c7dcfa0cab93f2e8ba2b22
poojadevesh <poojayadav.devesh@gmail.com>
2022-09-29 15:35:02 +0530

    regex url

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -3,11 +3,11 @@ const shortid = require('shortid');
 var validUrl = require('valid-url');
 
 
-const isValid = function(value) {
-    if (typeof value == "undefined" || value == null) return false;
-    if (typeof value == "string" && value.trim().length > 0) return true;
-    return false;
-};
+// const isValid = function(value) {
+//     if (typeof value == "undefined" || value == null) return false;
+//     if (typeof value == "string" && value.trim().length > 0) return true;
+//     return false;
+// };
 
 const isValidRequest = function(object) {
     return Object.keys(object).length > 0;
@@ -19,12 +19,13 @@ const shortenUrl = async(req,res)=>{
         if (!isValidRequest(data)) {
             return res.status(400).send({ status: false, message: "please provide data" });
         }
-        if (!isValid(data.longUrl)) {
-            return res.status(400).send({ status: false, message: "URL is mandatory" });
-        }
-        if (!validUrl.isUri(data.longUrl)) {
-            return res.status(400).send({ status: false, message: "URL is invalid" });
-        }
+       
+        const regex =
+      /^([hH][tT][tT][pP]([sS])?:\/\/.)(www\.)?[-a-zA-Z0-9@:%.\+#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%\+.#?&//=_]*$)/g;
+        if (typeof longUrl !== "string" && !regex.test(data.longUrl.trim()))
+      return res.status(400).send({ status: false, message: "Please provide Url and should be valid" });
+
+
         let presentUrl= await urlModel.findOne({longUrl:data.longUrl}).select("longUrl shortUrl urlCode")
         if(presentUrl){
             return res.status(200).send({ status: false, data:  presentUrl});
